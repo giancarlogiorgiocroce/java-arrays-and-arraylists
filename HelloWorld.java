f94ee25b68bbbe5f302b022bf50305b27c8656d0
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-10-20 15:01:18 +0200

    simple for

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 class HelloWorld {
     public static void main(String[]args){
-        System.out.println("HelloWorld");
+        // System.out.println("HelloWorld");
+
+        int number = 5;
+
+        for (int multiplier = 1; multiplier < 11; multiplier++){
+            System.out.println(number * multiplier);
+        }
     }
 }
